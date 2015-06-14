aee074602cc09d7d0a6939e65c8a7fe34dce7c4d
Marcel Voigt <mv@noch.so>
2015-06-14 13:26:27 +0200

    Forgot to pass original arguments to ff

--- ff.sh
+++ ff.sh
@@ -4,7 +4,7 @@ FFPHP="${FFPATH}cli-launch.php"
 FFTMP="${FFPATH}cli-launch.tmp"
 
 # Run fast-forward and capture output in $FFTMP
-php $FFPHP | tee $FFTMP
+php $FFPHP "$@" | tee $FFTMP
 
 # Get returned commands
 retcmd=$(grep -E "^cmd:.+" $FFTMP | sed 's/^cmd:\(.*\)/\1/')
