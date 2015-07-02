2b2bece1e30dc4be46eaf4b192391bed47c9a025
Marcel Voigt <mv@noch.so>
2015-07-02 21:43:43 +0200

    Replace $argv path to cli-launch.php with "ff"

--- cli-launch.php
+++ cli-launch.php
@@ -1,6 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 
+// Replace the path to *this* PHP file with the alias because the usage summary
+// relies on this.
+if ($argc > 0) {
+    $argv[0] = "ff";
+}
+
 $client = new \phparsenal\fastforward\Client();
 try {
     $client->init();
