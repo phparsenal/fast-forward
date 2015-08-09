bf5e0a69ed3dcc663c4a8cade98abaf960252200
Marcel Voigt <mv@noch.so>
2015-08-09 03:53:35 +0200

    Fix migration bug because of renaming database.version to ff.db.version

--- src/Migration.php
+++ src/Migration.php
@@ -85,7 +85,7 @@ class Migration
         // There's already a bookmark table, but no settings yet.
         // Basically the state of the project as this was written.
         $sql = '
-                CREATE TABLE "setting" (
+                CREATE TABLE IF NOT EXISTS "setting" (
                   "key" text NOT NULL,
                   "value" text NOT NULL
                 )';
